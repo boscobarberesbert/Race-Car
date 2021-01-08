3756c7281e95b10b0021efbb2316dcdad2b37d4d
Bosco Barber Esbert <boscobarberesbert@gmail.com>
2021-01-08 12:08:59 +0100

    First scene

--- ModuleSceneIntro.cpp
+++ ModuleSceneIntro.cpp
@@ -17,6 +17,22 @@ bool ModuleSceneIntro::Start()
 	LOG("Loading Intro assets");
 	bool ret = true;
 
+	// Building scenario
+	Cube* platform = new Cube(10, 10, 100);
+	platform->SetPos(0, 0, 0);
+	primitives.PushBack(platform);
+	App->physics->AddBody(*platform, 0.0f);
+
+	Cube* platform2 = new Cube(10, 10, 100);
+	platform2->SetPos(10, 10, 0);
+	primitives.PushBack(platform2);
+	App->physics->AddBody(*platform2, 0.0f);
+
+	Cube* platform3 = new Cube(10, 10, 100);
+	platform3->SetPos(-10, 10, 0);
+	primitives.PushBack(platform3);
+	App->physics->AddBody(*platform3, 0.0f);
+
 	return ret;
 }
 
@@ -34,6 +50,11 @@ update_status ModuleSceneIntro::Update(float dt)
 	Plane p(0, 1, 0, 0);
 	p.axis = true;
 	p.Render();
+
+	for (int i = 0; i < primitives.Count(); i++)
+	{
+		primitives[i]->Render();
+	}
 
 	return UPDATE_CONTINUE;
 }
--- ModuleSceneIntro.h
+++ ModuleSceneIntro.h
@@ -41,4 +41,6 @@ public:
 
 	PhysMotor3D* left_wheel;
 	PhysMotor3D* right_wheel;
+
+	p2DynArray<Primitive*> primitives;
 };
